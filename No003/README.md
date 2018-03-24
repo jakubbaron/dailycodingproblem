2210dd654cd3e19580caddf370b5e4aab9f63291
Jakub Baron <jakubaron@gmail.com>
2018-03-24 14:51:27 +0000

    Serialization and deserialization of the binary tree

--- No003/README.md
+++ No003/README.md
@@ -1,3 +1,6 @@
 python main.py
 
 ###Example output: </br></br>
+python main.py </br>
+Serialized node: {'right': "{'val': 'right'}", 'val': 'root', 'left': '{\'val\': \'left\', \'left\': "{\'val\': \'left.left\'}"}'}</br>
+From deserialized node: {'right': "{'val': 'right'}", 'val': 'root', 'left': '{\'val\': \'left\', \'left\': "{\'val\': \'left.left\'}"}'}</br>
--- No003/main.py
+++ No003/main.py
@@ -15,23 +15,47 @@
 #node = Node('root', Node('left', Node('left.left')), Node('right'))
 #assert deserialize(serialize(node)).left.left.val == 'left.left'
 
+class Node:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+  def __str__(self):
+    return str(serialize(self))
 
-def benchmark(method, name, runs, in_array):
-  import time
-  current_micro_time = lambda: int(round(time.time() * 1000000))
-  start_time = current_micro_time()
-  for i in range(0, runs):
-    output = method(in_array)
-  time_elapsed = current_micro_time() - start_time
+def serialize(root):
+  output = {}
+  output["val"] = str(root.val)
+  if root.left:
+    output["left"] = serialize(root.left)
+  if root.right:
+    output["right"] = serialize(root.right)
 
-  print str(name) + ": Output:" + str(output)
-  per_run = (time_elapsed/(runs * 1.0))
-  print str(name) + ": Per run: " + str(per_run) + "us"
+  return str(output)
 
+import ast
+def deserialize(data):
+  dictionary = ast.literal_eval(data)
+  val = dictionary.get("val", "")
+  left = None
+  if "left" in dictionary:
+    left = deserialize(dictionary["left"])
+  right = None
+  if "right" in dictionary:
+    right = deserialize(dictionary["right"])
+  return Node(val, left, right)
 
 def main():
-  in_array = [1, 2, 3, 4, 5]
-  runs = 1000
+  node = Node('root', Node('left', Node('left.left')), Node('right'))
+  print "Serialized node: " + str(serialize(node))
+
+  new_node = deserialize(serialize(node))
+  print "From deserialized node: " + str(serialize(new_node))
+
+  assert deserialize(serialize(node)).left.left.val == 'left.left'
+  assert deserialize(serialize(node)).right.val == 'right'
+  assert deserialize(serialize(node)).left.val == 'left'
+  assert deserialize(serialize(node)).val == 'root'
 
 if __name__ == "__main__":
   main()
