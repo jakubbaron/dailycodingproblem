db61427e31dd5e2978d00e1ac1da3c1241ca30e5
Jakub Baron <jakubaron@gmail.com>
2018-03-24 21:31:55 +0000

    Finding a missing positive integer in O(n) complexity and O(1) extra space

---
+++ No004/README.md
@@ -0,0 +1,8 @@
+python main.py
+
+###Example output: </br></br>
+❯ python main.py </br>
+Find missing positive integer: Output:2 </br>
+Find missing positive integer: Per run: 12.0us </br>
+Find missing positive integer: Output:3 </br>
+Find missing positive integer: Per run: 8.0us </br>
---
+++ No004/main.py
@@ -0,0 +1,68 @@
+#!/bin/python
+
+# Given an array of integers, find the first missing positive integer in linear time and constant space.
+# In other words, find the lowest positive integer that does not exist in the array.
+# The array can contain duplicates and negative numbers as well.
+#
+# For example, the input `[3, 4, -1, 1]` should give `2`. The input `[1, 2, 0]` should give `3`.
+#
+# You can modify the input array in-place.
+
+def swap(arr, id_i, id_j):
+  temp = arr[id_i]
+  arr[id_i] = arr[id_j]
+  arr[id_j] = temp
+
+def segregate(arr):
+  j = 0
+  for i in range(0, len(arr)):
+    if arr[i] <= 0:
+      swap(arr, i, j)
+      j += 1
+  return j
+
+def find_missing_positive_integer(in_arr):
+  split = segregate(in_arr)
+  new_size = len(in_arr) - split
+
+  for i in range(split, len(in_arr)):
+    j = i - split
+    el = abs(in_arr[j]) - 1
+    if el  < new_size and in_arr[el] > 0:
+      in_arr[el] = -in_arr[el]
+
+  for i in range(split, len(in_arr)):
+    j = i - split
+    if in_arr[j] > 0:
+      return j + 1
+  return new_size + 1
+
+
+def benchmark(method, name, runs, in_array, expected):
+  import time
+  current_micro_time = lambda: int(round(time.time() * 1000000))
+
+  start_time = current_micro_time()
+  for i in range(0, runs):
+    copy_array = in_array[:]
+    output = method(copy_array)
+  time_elapsed = current_micro_time() - start_time
+
+  print str(name) + ": Output:" + str(output)
+  per_run = (time_elapsed/(runs * 1.0))
+  print str(name) + ": Per run: " + str(per_run) + "us"
+
+  assert output == expected
+
+def main():
+  runs = 1
+  in_array = [3, 4, -1, 1]
+  expected = 2
+  benchmark(find_missing_positive_integer, "Find missing positive integer", runs, in_array, expected)
+
+  in_array = [1, 2, 0]
+  expected = 3
+  benchmark(find_missing_positive_integer, "Find missing positive integer", runs, in_array, expected)
+
+if __name__ == "__main__":
+  main()
