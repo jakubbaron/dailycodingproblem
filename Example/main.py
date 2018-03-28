ea247e953fff5988c85960d07f67a8d59f7fcb53
Jakub Baron <jakubaron@gmail.com>
2018-03-28 18:56:31 +0200

    Adding example directory from which to copy to new problems

---
+++ Example/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+def benchmark(method, name, runs, input, k):
+    import time
+    current_micro_time = lambda: int(round(time.time() * 1000000))
+    start_time = current_micro_time()
+    for i in range(0, runs):
+        output=method(input, k)
+    time_elapsed = current_micro_time() - start_time
+
+    per_run = (time_elapsed/(runs * 1.0))
+    print str(name) + ": Per run: " + str(per_run) + "us"
+
+    if output:
+        print str(name) + ": Found a pair"
+    else:
+        print str(name) + ": Pair not found"
+
+
+def main():
+  pass
+
+if __name__ == "__main__":
+    main()
