6449c724aae1f38dd9d78b377dd1a992a96a52ff
Jakub Baron <jakubaron@gmail.com>
2018-03-30 17:11:01 +0200

    Provided solution was for finding the biggest pair, it's supposed to be a biggest sum of nonconsecutive elements. Retain the pair solution and making a new one for a sequence

--- No009/main.py
+++ No009/main.py
@@ -11,40 +11,12 @@
 #
 # Follow-up: Can you do this in O(N) time and constant space?
 
-def brute_force(arr):
-  l = len(arr)
-  max_pair = (None, None)
-  max_sum = None
-  for i in range(0, l):
-    for j in range(0, l):
-      if j not in [i - 1, i, i + 1]:
-        s = arr[i] + arr[j]
-        if s > max_sum: #everything is greater than None
-          max_sum = s
-          max_pair = (arr[i], arr[j])
-  return max_pair
-
-def follow_up(arr):
+def brute_force:w(arr):
   pass
-
-def check_equal(pair1, pair2):
-  first1, second1 = pair1
-  first2, second2 = pair2
-  return (first1 == first2 and second1 == second2) or (first1 == second2 and second1 == first2)
-
 def main():
-  assert check_equal((4,8), (8,4))
-  assert check_equal((5,5), (5,5))
-
   arr = [2, 4, 6 ,8]
-  expected_pair = (4, 8)
-  assert check_equal(brute_force(arr), expected_pair)
-  #assert check_equal(follow_up(arr), expected_pair)
 
   arr = [5, 1, 1, 5]
-  expected_pair = (5, 5)
-  assert check_equal(brute_force(arr), expected_pair)
-  #assert check_equal(follow_up(arr), expected_pair)
 
 if __name__ == "__main__":
     main()
