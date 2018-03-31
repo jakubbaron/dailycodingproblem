35b26e4f6571f92162737d22d99526fdf8cc3879
Jakub Baron <jakubaron@gmail.com>
2018-03-31 18:33:17 +0200

    Adding a problem description

--- No011/main.py
+++ No011/main.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
+# Good morning. Here's your coding interview problem for today.
+# This problem was asked by Twitter.
+#
+# Implement an autocomplete system. That is, given a query string
+# s and a set of all possible query strings, return all strings
+# in the set that have s as a prefix.
+#
+# For example, given the query string de and the set of strings
+# [dog, deer, deal], return [deer, deal].
+#
+# Hint: Try preprocessing the dictionary into a more efficient data
+# structure to speed up queries.
+
+def build_up_dictionary(arr):
+  pass
+
+def autocomplete(input_string, dictionary):
+  pass
+
 def main():
+  arr = ['dog', 'deer', 'deal']
   pass
 
 if __name__ == "__main__":
