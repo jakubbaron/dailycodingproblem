f19a4cc0c7085e837c2569cf473b6a62a78300ad
Jakub Baron <jakubaron@gmail.com>
2018-04-02 21:34:01 +0200

    Adding a method's signature and tasks text

--- No013/main.py
+++ No013/main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+# Good morning. Here's your coding interview problem for today.
+# This problem was asked by Amazon.
+#
+# Given an integer k and a string s, find the length of the longest
+# substring that contains at most k distinct characters.
+#
+# For example, given s = "abcba" and k = 2, the longest substring
+# with k distinct characters is "bcb".
+
+def find_longest_substring(string, number_of_distinct_characters):
+  pass
 
 def main():
   pass
