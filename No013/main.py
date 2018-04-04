865550487a5538b9d07efbf4cc54886b13fcb75a
Jakub Baron <jakubaron@gmail.com>
2018-04-04 20:31:55 +0100

    Finding the longest substirng with at most distinct characters

--- No013/main.py
+++ No013/main.py
@@ -8,11 +8,31 @@
 # For example, given s = "abcba" and k = 2, the longest substring
 # with k distinct characters is "bcb".
 
+from collections import deque
+
 def find_longest_substring(string, number_of_distinct_characters):
-  pass
+  indices = deque(maxlen = number_of_distinct_characters + 1)
+  characters = deque(maxlen = number_of_distinct_characters + 1)
+  start, end = 0, 0
+  for i in range(0, len(string)):
+    character = string[i]
+    if character not in characters:
+      characters.append(character)
+      indices.append(i)
+    if len(characters) > number_of_distinct_characters:
+      first_char_idx = indices.popleft()
+      characters.popleft()
+    else:
+      first_char_idx = indices[0]
+
+    if (i - 1) - first_char_idx > end - start:
+      start, end = first_char_idx, i - 1
+
+  return string[start:end+1]
 
 def main():
-  pass
+  print str(find_longest_substring("abcba", 2))
+  assert find_longest_substring("abcba", 2) == "bcb"
 
 if __name__ == "__main__":
   main()
